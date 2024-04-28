d61c5559d49b90028e314b8a90bf17f706047f86
LeslyeGarcia <159669695+LeslyeGarcia@users.noreply.github.com>
2024-04-28 17:25:21 -0600

    Update event_screen.dart

---
+++ agenda/lib/event_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:prueba/event.dart';
+
+class EventScreen extends StatefulWidget {
+  EventScreen(Event? event);
+
+  @override
+  _EventScreenState createState() => _EventScreenState();
+}
+
+class _EventScreenState extends State<EventScreen> {
+  late TextEditingController _titleController;
+  late TextEditingController _descriptionController;
+  TimeOfDay _selectedTime = TimeOfDay.now();
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController();
+    _descriptionController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  void _saveEvent(BuildContext context) {
+    final String title = _titleController.text;
+    final String description = _descriptionController.text;
+
+    // Cerrar la pantalla de eventos y pasar los datos del evento a la pantalla anterior
+    Navigator.pop(context, {'title': title, 'description': description, 'time': _selectedTime});
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nuevo Evento"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 189, 140, 207), // Cambiar el color del AppBar
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [const Color.fromARGB(255, 205, 240, 208)!, Color.fromARGB(255, 215, 201, 220)!], // Cambiar los colores del fondo
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                labelText: "Título del evento",
+                filled: true,
+                fillColor: Color.fromARGB(255, 251, 255, 251), // Cambiar el color del campo de texto
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                labelText: "Descripción",
+                filled: true,
+                fillColor: Colors.white, // Cambiar el color del campo de texto
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            InkWell(
+              onTap: () => _selectTime(context),
+              child: InputDecorator(
+                decoration: InputDecoration(
+                  labelText: 'Hora del evento',
+                  filled: true,
+                  fillColor: Colors.white, // Cambiar el color del campo de texto
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hora del evento:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(_selectedTime.format(context)),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () => _saveEvent(context),
+              child: Text("Guardar"),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Color.fromARGB(255, 0, 0, 0), 
+                backgroundColor: Color.fromARGB(255, 132, 202, 172),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
