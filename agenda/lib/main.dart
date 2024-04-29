4446978356c16a23c6fd144065c06f16af3a143d
Gabriela Ramírez <159658876+abycan@users.noreply.github.com>
2024-04-29 00:30:36 -0600

    Update main.dart

--- agenda/lib/main.dart
+++ agenda/lib/main.dart
@@ -1,21 +1,81 @@
-import 'package:agenda/calendario.dart';
-import 'package:agenda/event.dart';
 import 'package:flutter/material.dart';
-import 'package:agenda/event_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:prueba/calendario.dart';
+import 'package:prueba/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  Event? get event => null;
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Agenda",
+      title: 'Agenda',
       debugShowCheckedModeBanner: false,
-      home: Calendar(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 205, 240, 208),
+                  Color.fromARGB(255, 205, 232, 236),
+                  Color.fromARGB(255, 209, 200, 238),
+                  Color.fromARGB(255, 235, 200, 212),
+                  Color.fromARGB(255, 215, 201, 220),
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Cargando...',
+              style: GoogleFonts.chilanka(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.transparent,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
